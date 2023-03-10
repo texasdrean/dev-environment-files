b4fd720f86197f7aaf72c949179021cb1911bc7f
texasdrean <andreteixeira@hotmail.fr>
2023-03-10 23:33:58 +0100

    Adding language server protocol (LSP)

---
+++ nvim/README.md
@@ -0,0 +1,8 @@
+# Neovim
+
+### To be created
+
+
+
+
+inpired by [josean](https://github.com/josean-dev) & docs
--- nvim/lua/texasdrean/plugins-setup.lua
+++ nvim/lua/texasdrean/plugins-setup.lua
@@ -53,6 +53,18 @@ return packer.startup(function(use)
     use("saadparwaiz1/cmp_luasnip")     -- allow nvim-cmp to show snippets on autocompletion
     use("rafamadriz/friendly-snippets") -- useful snippets
 
+    -- managing & installing lsp servers
+    use("williamboman/mason.nvim")              -- in charge of managing lsp servers, linters & formatters
+    use("williamboman/mason-lspconfig.nvim")    -- bridges gap b/w mason & lspconfig
+
+    -- configuring the lsp servers
+    use("neovim/nvim-lspconfig")        -- easily configure language servers
+    use("hrsh7th/cmp-nvim-lsp")         -- autocompletion
+    use({ "glepnir/lspsaga.nvim", branch = "main" })    -- enhanced lsp uis
+    use("jose-elias-alvarez/typescript.nvim")           -- additional functionality for typescript server (e.g. rename file & update imports)
+    use("onsails/lspkind.nvim")         -- vs-code like icons for autocompletion 
+
+
 
     -- fuzzy finding
     use {
