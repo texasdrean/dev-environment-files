0d393700ef39587e308863a40af91900081daaf2
texasdrean <andreteixeira@hotmail.fr>
2023-03-14 23:02:46 +0100

    Correction on keymaps config of shortcuts related to tabs

--- nvim/lua/texasdrean/core/keymaps.lua
+++ nvim/lua/texasdrean/core/keymaps.lua
@@ -28,9 +28,9 @@ keymap.set("n", "<leader>wd", "<C-w>j") -- move to the window up
 keymap.set("n", "<leader>wu", "<C-w>k") -- move to the window below
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabnew<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabnew<CR>") -- go to next tab
-keymap.set("n", "<leader>tp", ":tabnew<CR>") -- go to previous tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
 -- plugin keymaps
 -- vim-maximizer
